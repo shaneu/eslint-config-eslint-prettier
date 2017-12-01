5b052e4759ad6172dcbb429d7343784b2c88d621
Shane Unger <shane.unger1@gmail.com>
2017-12-01 14:49:45 -0700

    added support for flow

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,26 @@
 module.exports = {
   parser: "babel-eslint",
-  extends: ["airbnb", "prettier", "prettier/react"],
-  plugins: ["react", "prettier", "import"],
+  extends: [
+    "plugin:flowtype/recommended",
+    "airbnb",
+    "prettier",
+    "prettier/react",
+  ],
+  plugins: ["react", "flowtype", "prettier", "import"],
   parserOptions: {
     ecmaVersion: 8,
     sourceType: "module",
     ecmaFeatures: {
-      jsx: true
-    }
+      jsx: true,
+    },
   },
   env: {
     jest: true,
     browser: true,
     node: true,
-    es6: true
+    es6: true,
   },
   rules: {
-    "prettier/prettier": "error"
-  }
-};
+    "prettier/prettier": "error",
+  },
+}
